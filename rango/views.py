5c484353d298321f9d870d79c5980b24a1e96818
Connor <2470724c@student.gla.ac.uk>
2022-02-11 10:20:03 +0000

    CONFIXED 5(not 4)

--- rango/views.py
+++ rango/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rango.models import Category
 
 # Create your views here.
-from django.http import HttpResponse
 
 def index(request):
-    ''' construct a dictionary to pass to the template engine as its context.
-        Note the key boldmessage matches to {{ boldmessage }} in the template'''
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    ''' Query the database for a list of ALL categories currently stored.
+    Order the categories by the number of likes in descending order.
+    Retrieve the top 5 ONLY -- or all if there are less than 5
+    Place the list in our context_dict dictionary (with boldmessage)
+    that will be passed to the template engine.'''
+    #this queries the category model to retreive the top 5 categories
+    #the use of '-likes' notes that it is in decending order 'likes' would give ascending
+    #pythons list syntax then takes a subset of these up to 5, as specified
+    category_list = Category.objects.order_by('-likes')[:5]
 
-    ''' Return a rendered response to send to the client. 
-        We make use of the shortcut function to make our lives easier.
-        Note that the first parameter is the template we wish to use'''
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+
+    #render the response and send it back
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
